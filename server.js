9714570d9146135969b32c11c517b90691dd3e25
sarthak khandelwal <sarthak0906@gmail.com>
2019-04-04 23:47:15 +0530

    back-end start for project
    this is the forst commit for the backend of the project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+
+const app            = express();
+
+const port = 8000;
+
+require('./app/routes')(app, {});
+
+app.listen(port, () => {
+    console.log('we are live on ' + port);
+});
+
