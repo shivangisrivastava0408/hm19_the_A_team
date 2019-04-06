91325ed332374ccd2c9be60808594fd8a3fabf01
sarthak khandelwal <sarthak0906@gmail.com>
2019-04-06 13:03:05 +0530

    more content

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : "mongodb+srv://sarthak:5PACXF56eoE8rccQ@hm19-kvmqo.mongodb.net/test?retryWrites=true"
+    url     : "mongodb+srv://sarthak:5PACXF56eoE8rccQ@hm19-kvmqo.mongodb.net/test?retryWrites=true",
+    DB_NAME : "hm19_A" 
 }
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db.js');
 const app            = express();
-const DB_NAME        = "hm19_A" ;
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,14 +15,13 @@ app.listen(port, () => {
 MongoClient.connect(db.url, {useNewUrlParser: true}, (err, client) =>{
     if (err) return console.log(err)
 
-    database   = client.db(DB_NAME);
+    database   = client.db(db.DB_NAME);
     collection = database.collection("portal");
-    console.log("connected to database   : " + DB_NAME   );
-    console.log("connected to collection : " + collection);
-    // const portal = portal.db("note-api")
-    // require("./app/routes")(app, database);
-
-    // app.listen(port, () => {
-    //     console.log("we are live on " + port);
-    // });
+    var dbo    = {name: "sarthak",title: "this is a title"};
+    console.log("connected to database   : " + db.DB_NAME);
+    collection.insertOne(dbo, function(err, res) {
+        if (err) return console.log(err);
+        console.log("this is amazing");
+    })
+    // console.log(collection);
 })
